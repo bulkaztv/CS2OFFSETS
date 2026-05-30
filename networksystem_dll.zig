0b1b5cef6d65abe545dc458399c5884c58d39ff3
bulkaztv <94759998+bulkaztv@users.noreply.github.com>
2026-05-30 17:16:21 +0200

    Automatyczny upload: networksystem_dll.zig

---
+++ networksystem_dll.zig
@@ -0,0 +1,16 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-05-30 15:15:36.608743300 UTC
+
+pub const cs2_dumper = struct {
+    pub const schemas = struct {
+        // Module: networksystem.dll
+        // Class count: 1
+        // Enum count: 0
+        pub const networksystem_dll = struct {
+            // Parent: None
+            // Field count: 0
+            pub const ChangeAccessorFieldPathIndex_t = struct {
+            };
+        };
+    };
+};
