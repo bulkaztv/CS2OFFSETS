f877bf64820847e7ea2484c22e18c6d54e61d33b
bulkaztv <94759998+bulkaztv@users.noreply.github.com>
2026-05-30 17:16:07 +0200

    Automatyczny upload: host_dll.zig

---
+++ host_dll.zig
@@ -0,0 +1,20 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-05-30 15:15:36.608743300 UTC
+
+pub const cs2_dumper = struct {
+    pub const schemas = struct {
+        // Module: host.dll
+        // Class count: 2
+        // Enum count: 0
+        pub const host_dll = struct {
+            // Parent: xL_C__
+            // Field count: 0
+            pub const EmptyTestScript = struct {
+            };
+            // Parent: None
+            // Field count: 0
+            pub const CAnimScriptBase = struct {
+            };
+        };
+    };
+};
