0bc7c5a684ac67cada085710e077a0e5a8d5fde0
bulkaztv <94759998+bulkaztv@users.noreply.github.com>
2026-05-30 17:16:20 +0200

    Automatyczny upload: networksystem_dll.rs

---
+++ networksystem_dll.rs
@@ -0,0 +1,18 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-05-30 15:15:36.608743300 UTC
+
+#![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
+
+pub mod cs2_dumper {
+    pub mod schemas {
+        // Module: networksystem.dll
+        // Class count: 1
+        // Enum count: 0
+        pub mod networksystem_dll {
+            // Parent: None
+            // Field count: 0
+            pub mod ChangeAccessorFieldPathIndex_t {
+            }
+        }
+    }
+}
