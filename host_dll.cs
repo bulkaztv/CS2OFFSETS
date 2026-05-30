66afc5ba862f7b7294dd0464c5da9e1e37410cac
bulkaztv <94759998+bulkaztv@users.noreply.github.com>
2026-05-30 17:16:03 +0200

    Automatyczny upload: host_dll.cs

---
+++ host_dll.cs
@@ -0,0 +1,18 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-05-30 15:15:36.608743300 UTC
+
+namespace CS2Dumper.Schemas {
+    // Module: host.dll
+    // Class count: 2
+    // Enum count: 0
+    public static class HostDll {
+        // Parent: xL_C__
+        // Field count: 0
+        public static class EmptyTestScript {
+        }
+        // Parent: None
+        // Field count: 0
+        public static class CAnimScriptBase {
+        }
+    }
+}
